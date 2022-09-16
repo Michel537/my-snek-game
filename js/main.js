b6db387c95e353668dd1e5dd853223e400aa7c51
Michel Pomares <mapomarest@gmail.com>
2022-09-16 16:10:11 +0200

    Initial snake and classes

--- js/main.js
+++ js/main.js
@@ -1,1 +1,36 @@
-console.log("Hello world");
+class Game{
+    constructor(){
+
+    }
+
+    start(){
+        const snake = new Snake();
+        console.log(snake);
+    }
+}
+
+class Snake{
+    constructor(){
+        this.positionX = 50;
+        this.positionY = 50;
+        this.width = 5;
+        this.height = 5;
+        this.domElm = this.createDomElmnt();
+    }
+
+    createDomElmnt(){
+        const domElm = document.createElement("div");
+        domElm.id = "snake";        
+        domElm.style.width = this.width + "vw";
+        domElm.style.height = this.height + "vh";
+        domElm.style.bottom = this.positionX + "vw";
+        domElm.style.left = this.positionY + "vh"
+
+        const parentElm = document.getElementById("board");
+        parentElm.appendChild(domElm);
+
+    }
+}
+
+const game = new Game();
+game.start();
