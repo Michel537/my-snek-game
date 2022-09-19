6fb4326926e0acc08fab71425f6865c56e4bef13
Michel Pomares <mapomarest@gmail.com>
2022-09-19 14:36:07 +0200

    Basic movement done

--- js/main.js
+++ js/main.js
@@ -1,36 +1,98 @@
-class Game{
-    constructor(){
+class Game {
+  constructor() {
+    this.snake = new Snake();
+    
+  }
 
+  start() {   
+    
+    this.snake.move();     
+    
+    this.gameOver();
+
+  
+    
+    
+    
+  }
+
+  gameOver() {
+
+    if(this.snake.positionX >= 50 || this.snake.positionX <= 0){
+        console.log("we are inside");
+        return location.href = "gameover.html"        
+    }else if(this.snake.positionY <= 0 || this.snake.positionY >= 50 ){
+        console.log("we are inside");
+        return location.href = "gameover.html"        
     }
 
-    start(){
-        const snake = new Snake();
-        console.log(snake);
-    }
+  }
 }
 
-class Snake{
-    constructor(){
-        this.positionX = 50;
-        this.positionY = 50;
-        this.width = 5;
-        this.height = 5;
-        this.domElm = this.createDomElmnt();
-    }
+class Characters {
+  constructor() {
+    this.positionX;
+    this.positionY;
+    this.width;
+    this.height;
+  }
 
-    createDomElmnt(){
-        const domElm = document.createElement("div");
-        domElm.id = "snake";        
-        domElm.style.width = this.width + "vw";
-        domElm.style.height = this.height + "vh";
-        domElm.style.bottom = this.positionX + "vw";
-        domElm.style.left = this.positionY + "vh"
+  createDomElmnt(classElement) {
+    const domElm = document.createElement("div");
+    domElm.className = classElement;
+    domElm.style.width = this.width + "vw";
+    domElm.style.height = this.height + "vh";
+    domElm.style.bottom = this.positionY + "vh";
+    domElm.style.left = this.positionX + "vw";
 
-        const parentElm = document.getElementById("board");
-        parentElm.appendChild(domElm);
-
-    }
+    const parentElm = document.getElementById("board");
+    parentElm.appendChild(domElm);
+    return domElm;
+  }
 }
+
+class Snake extends Characters {
+  constructor() {
+    super();
+    this.positionX = 25;
+    this.positionY = 25;
+    this.width = 2;
+    this.height = 2;
+    this.domElm = this.createDomElmnt();
+  }
+
+  createDomElmnt() {
+    return super.createDomElmnt("snake");
+  }
+
+  moveOnDirection(direction) {}
+  move() {
+    const moveEvnt = document.addEventListener("keydown", (event) => {
+      if (event.key === "ArrowLeft") {
+        this.positionX -= this.width;
+        this.domElm.style.left = this.positionX + "vw";
+        console.log(this.positionX);
+      }
+      if (event.key === "ArrowRight") {
+        this.positionX += this.width;
+        this.domElm.style.left = this.positionX + "vw";
+        console.log(this.positionX);
+      }
+      if (event.key === "ArrowUp") {
+        this.positionY += this.height;
+        this.domElm.style.bottom = this.positionY + "vh";
+        console.log(this.positionY);
+      }
+      if (event.key === "ArrowDown") {
+        this.positionY -= this.height;
+        this.domElm.style.bottom = this.positionY + "vh";
+        console.log(this.positionY);
+      }
+    });
+  }
+}
+
+class Food {}
 
 const game = new Game();
 game.start();
