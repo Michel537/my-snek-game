eadb6245d040f307a8aaf240ef634c667220b27b
Michel Pomares <mapomarest@gmail.com>
2022-09-22 17:57:23 +0200

    Final css and small debuggin

--- js/main.js
+++ js/main.js
@@ -1,75 +1,64 @@
 class Game {
   constructor() {
-    this.board = this.createBoard();    
+    this.board = this.createBoard();
     this.ufo = new Ufo();
     this.score = 0;
     this.lvl = 1;
-    this.multiplayer = 1;    
-    this.asteroids = [];    
+    this.multiplayer = 1;
+    this.asteroids = [];
     this.fuel = null;
     this.speed = 100 / this.lvl;
     this.dificulty = 5000 / this.lvl;
   }
 
-  start() {
-    console.log(this.score);
+  start() {    
     this.fuel = this.createfuel();
     const obstacle = this.createasteroids();
     this.asteroids.push(obstacle);
 
-   const movementInt =  setInterval(() => { 
-
-      //Ufo movements      
+    const movementInt = setInterval(() => {
+      console.log("X"+this.ufo.positionX);
+      console.log("Y"+this.ufo.positionY);
+     
+    
+      //Ufo movements
       this.ufo.move();
       this.ufo.directionsEventListnr();
 
       //Collision
       let gameOver = this.gameOver();
-      this.asteroids.forEach( element => {
-        const collisionObst = this.detectCollision(element);  
-        if(collisionObst){          
-        location.href = "gameover.html";
-        }      
-
-      })
+      this.asteroids.forEach((element) => {
+        const collisionObst = this.detectCollision(element);
+        if (collisionObst) {
+          
+          location.href = "gameover.html";
+        }
+      });
 
       //Game over
-      if (gameOver) {        
+      if (gameOver) { 
+        /* alert("stop");  */      
         location.href = "gameover.html";
-      }
-      
+      } 
 
       //fuel and score
+      const scoreElmnt = document.getElementById("score");
       const collisonfuel = this.detectCollision(this.fuel);
-      if(collisonfuel){
-        this.score = this.score + (30 * this.multiplayer);
-        console.log(this.score);
+      if (collisonfuel) {
+        this.score = this.score + 30 * this.multiplayer;
+        scoreElmnt.innerText = this.score;
         this.removeElementofBoard(this.fuel);
         this.fuel = this.createfuel();
       }
-
-     
-     
     }, this.speed);
 
+    //Creating objects
 
-       //Creating objects
-
-       const obstInterval = setInterval(() => {
-          const obst = this.createasteroids();
-          this.asteroids.push(obst);                
-          
-        }, 3000);
-
-    
-
-    
-    
-   
-
-
+    const obstInterval = setInterval(() => {
+      const obst = this.createasteroids();
+      this.asteroids.push(obst);
+    }, 3000);
   }
- 
 
   createBoard() {
     const domElm = document.createElement("div");
@@ -96,7 +85,7 @@ class Game {
   }
 
   removeElementofBoard(fuel) {
-    const elmentToRmv = fuel.domElmnt;    
+    const elmentToRmv = fuel.domElmnt;
     fuel = null;
     elmentToRmv.remove();
   }
@@ -115,21 +104,24 @@ class Game {
     }
   }
 
-  gameOver() {
+   gameOver() {
     if (
-      this.ufo.positionX <= 0 ||
-      this.ufo.positionX + this.ufo.width >= 80
+      this.ufo.positionX   <= 0  ||
+      this.ufo.positionX + this.ufo.width/2 >= 80 
     ) {
       return true;
     } else if (
-      this.ufo.positionY <= 0 ||
-      this.ufo.positionY + this.ufo.height >= 80
+      this.ufo.positionY <= 0  ||
+      this.ufo.positionY + this.ufo.height/2 >= 80 
     ) {
       return true;
     } else {
       return false;
     }
   }
+
+  
+   
 }
 
 class Characters {
@@ -159,8 +151,8 @@ class Ufo extends Characters {
     super();
     this.positionX = 40;
     this.positionY = 40;
-    this.width = 3;
-    this.height = 3;
+    this.width = 4;
+    this.height = 4;
     this.direction = "left";
     this.domElmnt = super.addDomElement("ufo");
   }
@@ -183,14 +175,24 @@ class Ufo extends Characters {
   }
 
   move() {
+   /*  if (this.positionX  <= 0) {
+      this.positionX = 80;
+    } else if (this.positionX + 4 >= 80) {
+      this.positionX = 0;
+    } else if (this.positionY + 2 <= 0) {
+      this.positionY = 80;
+    } else if (this.positionY + 2 >= 80) {
+      this.positionY = 0;
+    }
+ */
     if (this.direction === "left") {
-      this.positionX -= this.width;
+      this.positionX -= 3;
     } else if (this.direction === "right") {
-      this.positionX += this.width;
+      this.positionX += 3;
     } else if (this.direction === "up") {
-      this.positionY += this.width;
+      this.positionY += 3;
     } else if (this.direction === "down") {
-      this.positionY -= this.width;
+      this.positionY -= 3;
     }
     this.domElmnt.style.left = this.positionX + "vw";
     this.domElmnt.style.bottom = this.positionY + "vh";
